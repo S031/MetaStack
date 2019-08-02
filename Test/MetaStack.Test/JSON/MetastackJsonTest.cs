7a4894bd1f0d9363ef080e2bb8ea85feeb03076c
Sergey <s031@ya.ru>
2019-08-02 12:12:15 +0300

    Add test stand

---
+++ Test/MetaStack.Test/JSON/MetastackJsonTest.cs
@@ -0,0 +1,49 @@
+﻿using S031.MetaStack.Common;
+using S031.MetaStack.Common.Logging;
+using S031.MetaStack.Json;
+using System;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+
+namespace MetaStack.Test.Json
+{
+	public class MetaStackJsonTest
+	{
+		private static readonly string _sourceJsonString = Encoding.UTF8.GetString(Resources.TestData.TestJson);
+		byte[] _sourceJsonData = Resources.TestData.TestJsonData;
+
+		public MetaStackJsonTest()
+		{
+			CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+			FileLogSettings.Default.Filter = (s, i) => i >= LogLevels.Debug;
+		}
+
+		[Fact]
+		public void PerformanceTest()
+		{
+			using (FileLog _logger = new FileLog(" MetaStackJson.PerformanceTest", new FileLogSettings() { DateFolderMask = "yyyy-MM-dd" }))
+			{
+				var str = _sourceJsonString;
+				_logger.Debug($"Start perfomance parse string test");
+				int i;
+				DateTime t = DateTime.Now;
+				for (i = 0; i < 10_000; i++)
+				{
+					var j = new JsonReader(ref str).Read();
+				}
+				_logger.Debug($"Finish perfomance parse string test. Time={(DateTime.Now-t).Milliseconds}ms, loop count={i}");
+
+				_logger.Debug($"Start perfomance ToString test");
+				t = DateTime.Now;
+				var json = new JsonReader(ref str).Read();
+				for (i = 0; i < 10_000; i++)
+				{
+					var s = json.ToString();
+				}
+				_logger.Debug($"Finish perfomance parse string test. Time={(DateTime.Now-t).Milliseconds}ms, loop count={i}");
+			}
+		}
+	}
+}
