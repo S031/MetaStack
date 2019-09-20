1df000c3946c0dedf4375a304b7fda57fc8bd6cd
Sergey <s031@ya.ru>
2019-09-20 17:16:58 +0300

    Add json well known types

--- src/S031.MetaStack.Core/Services/HostService/HostServiceOptions.cs
+++ src/S031.MetaStack.Core/Services/HostService/HostServiceOptions.cs
@@ -6,34 +6,29 @@ namespace S031.MetaStack.Core.Services
 {
     public class HostedServiceOptions
     {
-		private string _assemblyName;
-		private string _typeName;
+		private const int _delay = 60000;
 		private readonly Dictionary<string, object> _parameters = new Dictionary<string, object>();
 
-		private const int _delay = 60000;
+		public HostedServiceOptions() { }
 
-		public HostedServiceOptions()
-		{
-			Delay = _delay;
-		}
 		public HostedServiceOptions(string assemblyName, string typeName)
 		{
-			_assemblyName = assemblyName;
-			_typeName = typeName;
-			Delay = _delay;
+			AssemblyName = assemblyName;
+			TypeName = typeName;
 		}
 
-		public string AssemblyName { get => _assemblyName; set { _assemblyName = value; } }
+		public string AssemblyName { get; set; }
 
-		public string TypeName { get => _typeName; set { _typeName = value; } }
+		public string TypeName { get; set; }
 
+		public string Name => TypeName.Replace('.', '_');
 
-		public string Name => _typeName.Replace('.', '_');
-
-		public int Delay { get; set; }
+		public int Delay { get; set; } = _delay;
 
 		public string UserName { get; set; }
+
 		public string Password { get; set; }
+
 		public string LogName { get; set; }
 
 		public Common.Logging.FileLogSettings LogSettings { get; set; }
