4ae2d4bf5e392e9590b1fdab5dfe9979f5567011
Sergey <s031@ya.ru>
2019-10-03 18:03:18 +0300

    fix small

--- src/S031.MetaStack.Core/Json/JSONExtensions.cs
+++ src/S031.MetaStack.Core/Json/JSONExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using MessagePack;
+using S031.MetaStack.Common;
+using S031.MetaStack.Json;
 
 #if NETCOREAPP
 namespace S031.MetaStack.Core.Json
@@ -9,14 +11,39 @@ namespace S031.MetaStack.WinForms.Json
 {
 	public static class JSONExtensions
     {
- 
-		public static object DeserializeObject(string value)
-			=>MessagePackSerializer.Typeless.Deserialize(MessagePack.MessagePackSerializer.FromJson(value));
+
+		public static object DeserializeObject(Type t, string value)
+		{
+			if (typeof(JsonSerializible).IsAssignableFrom(t))
+			{
+				JsonValue jsonValue = new JsonReader(ref value).Read();
+				return t.CreateInstance(jsonValue);
+			}
+			else if (JsonWellKnownTypes.TryGetValue(t, out var f))
+			{
+				JsonValue jsonValue = new JsonReader(ref value).Read();
+				var instance = t.CreateInstance();
+				f.ReadDelegate(jsonValue, instance);
+				return instance;
+
+			}
+			return MessagePackSerializer.Typeless.Deserialize(MessagePack.MessagePackSerializer.FromJson(value));
+		}
 
 		public static T DeserializeObject<T>(string value)
-			=> (T)MessagePackSerializer.Deserialize<T>(MessagePack.MessagePackSerializer.FromJson(value));
+			=> (T)DeserializeObject(typeof(T), value);
 
 		public static string SerializeObject(object value)
-			=> MessagePack.MessagePackSerializer.ToJson(MessagePackSerializer.Typeless.Serialize(value));
+		{
+			if (value is JsonSerializible js)
+				return js.ToString(Formatting.None);
+			else if (JsonWellKnownTypes.TryGetValue(value.GetType(), out var f))
+			{
+				JsonWriter writer = new JsonWriter(Formatting.None);
+				f.WriteDelegate(writer, value);
+				return writer.ToString();
+			}
+			return MessagePack.MessagePackSerializer.ToJson(MessagePackSerializer.Typeless.Serialize(value));
+		}
 	}
 }
