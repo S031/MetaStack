1d3c634881fda6b72fef97bb585ac60592359248
Sergey <s031@ya.ru>
2020-01-24 12:38:26 +0300

    Fix sulution errors

--- src/S031.MetaStack.Buffers/Properties/AssemblyInfo.cs
+++ src/S031.MetaStack.Buffers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("S031.MetaStack.Buffers.Net45")]
+[assembly: AssemblyTitle("S031.MetaStack.Buffers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("S031.MetaStack.Buffers.Net45")]
+[assembly: AssemblyProduct("S031.MetaStack.Buffers")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
