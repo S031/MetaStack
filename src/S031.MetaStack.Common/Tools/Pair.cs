f09bf6e05b4cbc7fbd51ee1a63f3399450709fe2
Sergey <s031@ya.ru>
2020-02-14 16:42:55 +0300

    Fix Pair structure

--- src/S031.MetaStack.Common/Tools/Pair.cs
+++ src/S031.MetaStack.Common/Tools/Pair.cs
@@ -5,14 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace S031.MetaStack.Common
-{	public ref struct Pair<T>
+{	public readonly ref struct Pair<T>
 	{
 		public Pair(T xValue, T yValue)
 		{
 			x = xValue;
 			y = yValue;
 		}
-		public T x;
-		public T y;
+		public T x { get; }
+		public T y { get; }
 	}
 }
