57bd4c165d12c1010b5248e7d54092b5da75e819
Sergey <s031@ya.ru>
2020-03-25 18:48:44 +0300

    Create ORM project

---
+++ src/S031.Metastack.ORM/JMXKeyMember.cs
@@ -0,0 +1,74 @@
+﻿using S031.MetaStack.Json;
+
+namespace S031.MetaStack.ORM
+{
+	public sealed class JMXKeyMember: JsonSerializible
+	{
+        public JMXKeyMember() : base(null) { }
+
+        public string FieldName { get; set; }
+        public int Position { get; set; }
+        public bool IsDescending { get; set; }
+        public bool IsIncluded { get; set; }
+		public static bool operator ==(JMXKeyMember m1, JMXKeyMember m2)
+		{
+			return (m1.FieldName == m2.FieldName &&
+				m1.Position == m2.Position &&
+				m1.IsDescending == m2.IsDescending &&
+				m1.IsIncluded == m2.IsIncluded);
+		}
+		public static bool operator !=(JMXKeyMember m1, JMXKeyMember m2)
+		{
+			return (m1.FieldName != m2.FieldName ||
+				m1.Position != m2.Position ||
+				m1.IsDescending != m2.IsDescending ||
+				m1.IsIncluded != m2.IsIncluded);
+		}
+		public override bool Equals(object obj)
+		{
+			return this.GetHashCode() == obj.GetHashCode();
+		}
+		public override int GetHashCode()
+		{
+			return new { FieldName, Position, IsDescending, IsIncluded }.GetHashCode();
+		}
+		public bool IsEmpty() => string.IsNullOrEmpty(FieldName);
+
+        public override string ToString()
+            => this.ToString(Formatting.None);
+
+        public override string ToString(Formatting formatting)
+        {
+            JsonWriter writer = new JsonWriter(formatting);
+            ToJson(writer);
+            return writer.ToString();
+        }
+
+        internal JMXKeyMember(JsonObject o)
+            :base(o)
+		{
+			FieldName = o.GetStringOrDefault("FieldName");
+			Position = o.GetIntOrDefault("Position");
+			IsDescending = o.GetBoolOrDefault("IsDescending");
+			IsIncluded = o.GetBoolOrDefault("IsIncluded");
+		}
+
+		internal static JMXKeyMember ReadFrom(JsonObject o)
+			=> new JMXKeyMember(o);
+
+        public override void ToJson(JsonWriter writer)
+        {
+			writer.WriteStartObject();
+            ToJsonRaw(writer);
+			writer.WriteEndObject();
+        }
+
+        protected override void ToJsonRaw(JsonWriter writer)
+        {
+			writer.WriteProperty("FieldName", FieldName);
+			writer.WriteProperty("Position", Position);
+			writer.WriteProperty("IsDescending", IsDescending);
+			writer.WriteProperty("IsIncluded", IsIncluded);
+        }
+    }
+}
---
+++ src/S031.Metastack.ORM/JMXObjectName.cs
@@ -0,0 +1,70 @@
+﻿using S031.MetaStack.Common;
+using System;
+
+namespace S031.MetaStack.ORM
+{
+	public readonly struct JMXObjectName
+	{
+		internal JMXObjectName(string fullObjectName)
+		{
+			int pos = fullObjectName.IndexOf('.');
+			if (pos > -1)
+			{
+				AreaName = fullObjectName.Left(pos);
+				ObjectName = fullObjectName.Substring(pos + 1);
+			}
+			else
+			{
+				AreaName = string.Empty;
+				ObjectName = fullObjectName;
+			}
+		}
+		public JMXObjectName(string areaName, string objectName)
+		{
+			AreaName = areaName;
+			ObjectName = objectName;
+		}
+		public string AreaName { get; }
+		public string ObjectName { get; }
+		public static bool operator ==(JMXObjectName m1, JMXObjectName m2)
+		{
+			return (m1.AreaName == m2.AreaName &&
+				m1.ObjectName == m2.ObjectName);
+		}
+		public static bool operator !=(JMXObjectName m1, JMXObjectName m2)
+		{
+			return (m1.AreaName != m2.AreaName ||
+				m1.ObjectName != m2.ObjectName);
+		}
+		public override bool Equals(object obj)
+		{
+			return this.ToString().Equals(obj.ToString(), StringComparison.Ordinal);
+		}
+		public override int GetHashCode()
+		{
+			return new { AreaName, ObjectName }.GetHashCode();
+		}
+		public override string ToString()
+		{
+			return this.ToString(false);
+		}
+		public string ToString(bool withQuotes)
+		{
+			if (withQuotes)
+				return $"[{AreaName}].[{ObjectName}]";
+			return $"{AreaName}.{ObjectName}";
+		}
+		public static implicit operator JMXObjectName(string s)
+		{
+			return new JMXObjectName(s);
+		}
+		public static implicit operator string(JMXObjectName name)
+		{
+			return name.ToString();
+		}
+		public bool IsEmpty()
+		{
+			return ObjectName.IsEmpty();
+		}
+	}
+}
