230561243c590c62caa74abf8f795f639b3dba60
Sergey <s031@ya.ru>
2020-10-16 19:27:06 +0300

    Optimize project files

--- src/S031.MetaStack.Buffers/BinaryDataBuffer.cs
+++ src/S031.MetaStack.Buffers/BinaryDataBuffer.cs
@@ -2,11 +2,7 @@
 #if NETCOREAPP
 using System.Buffers;
 #endif
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace S031.MetaStack.Buffers
 {
